9ed4d0493cbcdfb83b5c2839328d2e91d79e5696
Kiet Ho <hoakiet98@gmail.com>
2020-01-19 15:04:45 -0600

    implemented waking mechanism

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "adc26d4cc0e39cd3222a";
+/******/ 	var hotCurrentHash = "cde91eac8d2b9ce9654d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const next = require('next')
 const proxy = require('express-http-proxy');
+const wakeUpDyno = require("./utils/wakeDyno.js"); // my module!
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const dyno_url = "https://madebykiet.herokuapp.com"; // the url of your dyno
 
 app.prepare().then(() => {
   const server = express()
@@ -19,5 +21,7 @@ app.prepare().then(() => {
   server.listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
+    wakeUpDyno(dyno_url);
+
   })
 })
